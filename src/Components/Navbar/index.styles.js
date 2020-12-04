059275bf65dd00fa68bd94c1ef926a97bd752c50
alexmwaura <alexmwaura43@gmail.com>
2020-12-04 23:09:06 +0300

    add changes

---
+++ src/Components/Navbar/index.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const MainDiv = styled.div`
+    border-bottom-color:#000;
+    height: '100px';
+
+`
---
+++ src/Components/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+import Footer from "./Footer"
+
+export default {
+    Navbar,
+    Sidebar,
+    Footer
+}
