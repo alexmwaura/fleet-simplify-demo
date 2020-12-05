a882c6ed149644549c6f1d3a5a5c7b8c5627ce81
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 03:31:30 +0300

    add styled component for sidebar grid

--- src/Components/Sidebar/index.styles.js
+++ src/Components/Sidebar/index.styles.js
@@ -3,3 +3,7 @@ import styled from "styled-components";
 export const SideBar = styled.div`
   height: fit-content;
 `;
+export const SideBarGrid = styled.div`
+  background: #80147f;
+  width: 260px;
+`;
