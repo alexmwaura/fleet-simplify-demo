2e9960cd0fa8aa54f4428f4b5f5985629db98a3e
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 03:07:31 +0300

    add footer section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import Components from "./Components";
+
 function App() {
   return (
     <>
       <Components.Navbar />
-      <div className="container" style={{marginTop: '100px'}}>
+      <div>
         <div className="row">
-          <div className="col-md-2">
+          <div
+            className="col-md-2"
+            style={{ background: "#80147f", width: "260px" }}
+          >
             <Components.Sidebar />
           </div>
           <div className="col-md-10">
             <p>body</p>
           </div>
         </div>
+        <Components.Footer />
       </div>
     </>
   );
