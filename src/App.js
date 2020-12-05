9ec465d889bac0a0083bd224a3e16a26f4d31b66
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 03:30:02 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ function App() {
   return (
     <>
       <Components.Navbar />
-      <div>
         <div className="row">
-          <div
-            className="col-md-2"
-            style={{ background: "#80147f", width: "260px" }}
-          >
-            <Components.Sidebar />
-          </div>
-          <div className="col-md-10">
-            <p>body</p>
-          </div>
+          <Components.Sidebar />
+          <Components.MainGridLayout />
         </div>
         <Components.Footer />
-      </div>
     </>
   );
 }
