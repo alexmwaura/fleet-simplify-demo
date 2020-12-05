f3cfd9a2d0f33964d04779bf81218a65157fdf23
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 15:45:44 +0300

    add inbox window

---
+++ src/Components/MainGridLayout/InboxWindow/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./index.css";
+import InboxWindow from "./inboxWindow";
+
+export default function MainInboxWindow() {
+  return (
+    <div className="col-md-5">
+      <div className="chat_list">
+        <div className="container">
+          <div className="inbox_chat" style={{ backgroundColor: "rgb(193 217 245)", border: "1px #f5e556 solid",borderTop: "none"}}>
+            <InboxWindow />
+          </div>
+          <br />
+          <br />
+          <div className="inbox_chat" style={{ backgroundColor: "#e9ecef", border: "1px rgb(232 221 221) solid",borderTop: "none"}}>
+            <InboxWindow />
+          </div>
+          <br />
+          <br />
+          <div className="inbox_chat" style={{ backgroundColor: "rgb(193 217 245)", border: "1px #f5e556 solid",borderTop: "none"}}>
+            <InboxWindow />
+          </div>
+          <br />
+          <br />
+          <div className="inbox_chat" style={{ backgroundColor: "rgb(193 217 245)", border: "1px #f5e556 solid",borderTop: "none"}}>
+            <InboxWindow />
+          </div>
+          <br />
+          <br />
+          <div className="inbox_chat" style={{ backgroundColor: "#e9ecef", border: "1px rgb(232 221 221) solid",borderTop: "none"}}>
+            <InboxWindow />
+          </div>
+          <br />
+          <br />
+          <div className="inbox_chat">
+            <InboxWindow />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
