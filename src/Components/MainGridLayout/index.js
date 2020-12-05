28211278f8fb5b39bfd452548397d578809b66bb
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 04:57:00 +0300

    add search component

--- src/Components/MainGridLayout/index.js
+++ src/Components/MainGridLayout/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import SearchBar from "./SearchBar"
 
 export default function MainGridLayout() {
   return (
-    <div className="col-md-10">
-      <p>body</p>
+    <div className="col-md-10 col-md-10 mx-auto">
+        <SearchBar />
     </div>
   );
 }
