303069d981af0b1a175a44a7e06dfc7c230f2cbb
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 15:45:10 +0300

    add inbox window

--- src/Components/MainGridLayout/index.js
+++ src/Components/MainGridLayout/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import SearchBar from "./SearchBar"
+import InboxWindow from "./InboxWindow"
+import MessageBox from "./MessageBox"
 
 export default function MainGridLayout() {
   return (
-    <div className="col-md-10 col-md-10 mx-auto">
+    <div className="col-md-10 col-md-10 mx-auto" style={{marginBottom: '100px'}}>
         <SearchBar />
+          <div className="row">
+            <InboxWindow/>
+            <MessageBox/>
+          </div>
+        
     </div>
   );
 }
