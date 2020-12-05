1f29ab0c3a4ba2a3ada57c94eb20eef8c12a150e
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 03:07:54 +0300

    format doc

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
-import Navbar from "./Navbar"
-import Sidebar from "./Sidebar"
-import Footer from "./Footer"
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 
 export default {
-    Navbar,
-    Sidebar,
-    Footer
-}
+  Navbar,
+  Sidebar,
+  Footer,
+};
