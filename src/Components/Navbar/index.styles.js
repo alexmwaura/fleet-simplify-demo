96d25419858d98738105660e9292078045bfb266
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 03:09:02 +0300

    remove height

--- src/Components/Navbar/index.styles.js
+++ src/Components/Navbar/index.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const MainDiv = styled.div`
     border-bottom-color:#000;
-    height: '100px';
+    background-color: #fff;
 `
 
 export const ListAnchor = styled.a``
