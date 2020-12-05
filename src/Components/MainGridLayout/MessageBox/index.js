e478d6ee2f23c05350abe9bcd29aa652b4926495
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 17:36:21 +0300

    add message header and window

--- src/Components/MainGridLayout/MessageBox/index.js
+++ src/Components/MainGridLayout/MessageBox/index.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React from "react";
+import "./index.css";
+import MessageHeader from "./MessageHeader";
+import MessageWindow from "./MessageWindow"
 
-export default function MessageBox(){
-    return(
-        <div className="col-md-7">
-            Message Box
-        </div>
-    )
+export default function MessageBox() {
+  return (
+    <div className="col-md-7">
+      <div className="container wrapper">
+        <MessageHeader />
+        <MessageWindow />
+      </div>
+    </div>
+  );
 }
