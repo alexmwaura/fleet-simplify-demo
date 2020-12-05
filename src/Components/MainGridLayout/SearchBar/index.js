a0e5b32f40d2061376cf358bfbf9e1fa2b266d66
alexmwaura <alexmwaura43@gmail.com>
2020-12-05 04:57:30 +0300

    update search bar

---
+++ src/Components/MainGridLayout/SearchBar/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { SearchGrid } from "./index.styles";
+import "./index.css";
+
+export default function SearchBar() {
+  return (
+    <div className="container">
+      <div className="row">
+        <SearchGrid className="col-md-6 col-md-4 mx-auto">
+            <ul className="list-inline">
+          <li className="list-inline-item">
+          <form>
+            <div className="p-1 bg-light rounded rounded-pill shadow-sm mb-4" style={{borderRadius: '2px solid yellow'}}>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <button
+                    type="submit"
+                    id="button-addon"
+                    className="btn btn-link text-dark"
+                  >
+                    <i className="fa fa-search"></i>
+                  </button>
+                </div>
+                <input
+                  type="search"
+                  placeholder="Search user messages or chat id's"
+                  aria-describedby="button-addon"
+                  className="form-control border-0 bg-light"
+                />
+              </div>
+            </div>
+          </form>
+          </li>
+          &nbsp;&nbsp;&nbsp;
+          <li className="list-inline-item">
+              <button className="btn btn-link text-dark" type="submit">
+              <i class="fas fa-sort-amount-down"></i>
+              </button>
+          </li>
+          </ul>
+        </SearchGrid>
+        <SearchGrid className="col-md-6 float-sm-right">
+          <div className="float-sm-right">
+            <button className="btn btn-warning">
+              <i className="fas fa-plus"></i>Start a new chat
+            </button>
+          </div>
+        </SearchGrid>
+      </div>
+    </div>
+  );
+}
