e92ae2ce1102a885f59cab2af9d27baee8287b82
alexmwaura <alexmwaura43@gmail.com>
2020-12-06 22:43:07 +0300

    add styled component

---
+++ src/Components/MainGridLayout/MessageBox/MessageHeader/index.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const DropDownMenu = styled.div`
+  border: 0;
+  background: inherit;
+`;
+
+export const CardHeader = styled.div`
+  text-align: center;
+  margin-top: -8px;
+`;
+export const MainRow = styled.div`
+  background-color: rgb(193 217 245);
+  border-top: none;
+  height: auto;
+  box-shadow: 1px 1px 3px;
+  margin-right: 0px;
+  margin-left: 0px;
+`;
