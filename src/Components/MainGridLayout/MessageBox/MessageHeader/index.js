68918a50aaf793cbd0a15edb12e38515b0b01b79
alexmwaura <alexmwaura43@gmail.com>
2020-12-06 22:42:53 +0300

    remove inline styles

--- src/Components/MainGridLayout/MessageBox/MessageHeader/index.js
+++ src/Components/MainGridLayout/MessageBox/MessageHeader/index.js
@@ -1,20 +1,11 @@
 import React from "react";
 import ChatActions from "./ChatActions";
 import ContactInfo from "./ContactInfo";
+import { MainRow } from "./index.styles";
 
 export default function MessageHeader() {
   return (
-    <div
-      className="row"
-      style={{
-        backgroundColor: "rgb(193 217 245)",
-        borderTop: "none",
-        height: "auto",
-        boxShadow: "1px 1px 3px",
-        marginRight: "0px",
-        marginLeft: "0px",
-      }}
-    >
+    <MainRow className="row">
       <div className="col-md-2 user-status">
         <img
           src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg"
@@ -37,6 +28,6 @@ export default function MessageHeader() {
           </li>
         </ul>
       </div>
-    </div>
+    </MainRow>
   );
 }
