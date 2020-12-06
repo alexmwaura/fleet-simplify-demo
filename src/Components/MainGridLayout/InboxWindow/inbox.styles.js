1fcdc30e4e17550e59c8eb78b709c89f22e8c585
alexmwaura <alexmwaura43@gmail.com>
2020-12-06 22:24:20 +0300

    add styled component

---
+++ src/Components/MainGridLayout/InboxWindow/inbox.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const InboxChatActive = styled.div`
+  background-color: rgb(193 217 245);
+  border: 1px #f5e556 solid;
+  border-top: none;
+  margin-bottom: 30px;
+`;
+export const InboxChatDisabled = styled.div`
+  background-color: #e9ecef;
+  border: 1px rgb(232 221 221) solid;
+  border-top: none;
+  margin-bottom: 30px;
+`;
